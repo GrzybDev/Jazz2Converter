a6d581ea4d46f153ee85791c3bac636423b167df
Marek Grzyb <grzybdev@gmail.com>
2019-12-23 19:04:02 +0100

    Added showHelp function

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,21 @@
 import logging
 
+from src.errors import SUCCESS_OK, ERROR_INVALID_ARGUMENTS, ERROR_UNKNOWN_ARGUMENT
 from src.logger import error, info
+
 logging.basicConfig(format="[%(module)s] %(message)s")
 
 
-def run():
-    logging.info()
-    return 0
+def showHelp():
+    logging.info(info("Usage: run.py [-h|--help] -i|--input \"GAME FOLDER\" (arguments)"))
+    logging.info("\n")
+    logging.info(info("-h | --help || Shows this message"))
+    logging.info(info("-v | --verbose || Shows additional debug information"))
+    logging.info(info("-i | --input || Sets Jazz Jackrabbit 2 game folder"))
+    logging.info("\n")
+    logging.info(info("Converter options:"))
+    logging.info(info("--skip-languages || Skips language files (*.j2s)"))
+    return SUCCESS_OK
 
 
 if __name__ == "__main__":
