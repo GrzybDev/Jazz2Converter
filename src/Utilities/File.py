50289967de38f2cac16dc0e0201fcb0ebf5b7863
Marek Grzyb <grzybdev@gmail.com>
2019-12-27 17:31:56 +0100

    Moved some files from main source folder to seperate folders

---
+++ src/Utilities/File.py
@@ -0,0 +1,16 @@
+from struct import unpack
+
+
+class File(object):
+
+    def __init__(self, fileHandle):
+        self.context = fileHandle
+
+    def ReadBytes(self, count):
+        return self.context.read(count)
+
+    def ReadChar(self):
+        return unpack("B", self.context.read(1))[0]
+
+    def ReadUInt(self):
+        return unpack("I", self.context.read(4))[0]
