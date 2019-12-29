c3fc4a9d0db5903afa238c4cbf43637559c57679
Marek Grzyb <grzybdev@gmail.com>
2019-12-29 16:41:16 +0100

    Added __init__ file in DataAssets

---
+++ src/Formats/DataAssets/__init__.py
@@ -0,0 +1,4 @@
+from src.Formats.DataAssets.Picture import PictureDataFile
+from src.Formats.DataAssets.Palette import PaletteDataFile
+from src.Formats.DataAssets.SoundFXList import SoundFXList
+from src.Formats.DataAssets.Texture import TextureDataFile
