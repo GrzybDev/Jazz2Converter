e1e13aa77db589310af171dcab86c9765be841aa
Marek Grzyb <grzybdev@gmail.com>
2019-12-29 16:41:51 +0100

    Added knownFileTypes in Data archives

--- src/Formats/Data.py
+++ src/Formats/Data.py
@@ -3,10 +3,31 @@ import zlib
 
 from src.DataClasses.Data import *
 from src.Helpers.logger import *
-from src.Utilities.FileConverter import FileConverter
+from src.Utilities import FileConverter
+from src.Formats.DataAssets import *
 
 
 class DataConverter(FileConverter):
+
+    knownFileTypes = {
+        0x23: PictureDataFile,
+        0x18: PictureDataFile,
+        0x1D: PictureDataFile,
+        0x17: PictureDataFile,
+        0x16: PictureDataFile,
+        0x19: PictureDataFile,
+        0x1A: PictureDataFile,
+        0x15: PictureDataFile,
+        0x52: PictureDataFile,
+        0x9: PaletteDataFile,
+        0xFFFFFFFF: PaletteDataFile,
+        0x11C9A10: SoundFXList,
+        0x11C8330: SoundFXList,
+        0x11C88A0: SoundFXList,
+        0x11C8750: SoundFXList,
+        0x11C8320: SoundFXList,
+        0x11C8AB0: TextureDataFile
+    }
 
     def __init__(self, path):
         super().__init__(path)
