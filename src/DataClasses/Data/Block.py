e16808a2e8af330febc286f69929391796d6355e
Marek Grzyb <grzybdev@gmail.com>
2019-12-31 13:46:18 +0100

    Replaced my weird workaround for reading DataBlocks with BytesIO

--- src/DataClasses/Data/Block.py
+++ src/DataClasses/Data/Block.py
@@ -1,22 +1,29 @@
+from io import BytesIO
+
 from struct import unpack
 
 
 class DataBlock:
 
     def __init__(self, data):
-        self.data = data
+        self.context = BytesIO(data)
 
     def ReadString(self, length, trimToNull=False):
-        result = self.data[:length]
+        result = self.context.read(length)
 
         if trimToNull:
             result = result.split(b"\x00")[0]
-        
-        self.data = self.data[length:]
+
         return result.decode()
     
     def ReadUInt(self):
-        result = unpack("I", self.data[:4])
-        self.data = self.data[4:]
+        t = unpack("I", self.context.read(4))[0]
+        return t
 
-        return result[0]
+    def ReadUShort(self):
+        t = unpack("H", self.context.read(2))[0]
+        return t
+
+    def ReadByte(self):
+        t = unpack("B", self.context.read(1))[0]
+        return t
