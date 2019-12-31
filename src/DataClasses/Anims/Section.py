82af6969b7ad4f00a73fe15a13c702af11b0168c
Marek Grzyb <grzybdev@gmail.com>
2019-12-31 13:45:15 +0100

    Added Section DataClass

---
+++ src/DataClasses/Anims/Section.py
@@ -0,0 +1,15 @@
+class Section:
+    ID = 0
+
+    FrameCount = 0
+    FrameRate = 0
+    Frames = []
+
+    NormalizedHotspotX = 0
+    NormalizedHotspotY = 0
+
+    LargestOffsetX = 0
+    LargestOffsetY = 0
+
+    AdjustedSizeX = 0
+    AdjustedSizeY = 0
