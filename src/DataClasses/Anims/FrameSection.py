0bfa16b0adc79f291a43fbe56e186d7369b3350a
Marek Grzyb <grzybdev@gmail.com>
2019-12-31 13:45:03 +0100

    Added FrameSection DataClass

---
+++ src/DataClasses/Anims/FrameSection.py
@@ -0,0 +1,17 @@
+class FrameSection:
+    SizeX = 0
+    SizeY = 0
+
+    ColdspotX = 0
+    ColdspotY = 0
+
+    HotspotX = 0
+    HotspotY = 0
+
+    GunspotX = 0
+    GunspotY = 0
+
+    ImageAddr = 0
+    MaskAddr = 0
+
+    DrawTransparent = False
