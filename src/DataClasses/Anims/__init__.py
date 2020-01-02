85d84ceccd543fa35c21968e8515462518441764
Marek Grzyb <grzybdev@gmail.com>
2020-01-02 12:19:44 +0100

    Imported SampleSection in __init__

--- src/DataClasses/Anims/__init__.py
+++ src/DataClasses/Anims/__init__.py
@@ -1,3 +1,4 @@
 from src.DataClasses.Anims.Set import Set
 from src.DataClasses.Anims.Section import Section
 from src.DataClasses.Anims.FrameSection import FrameSection
+from src.DataClasses.Anims.SampleSection import SampleSection
