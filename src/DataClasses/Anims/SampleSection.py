067a5b79876aecc0ff29b450f6d603164f7246a1
Marek Grzyb <grzybdev@gmail.com>
2020-01-02 12:19:28 +0100

    Added SampleSection DataClass

---
+++ src/DataClasses/Anims/SampleSection.py
@@ -0,0 +1,13 @@
+class SampleSection:
+    IdInSet = 0
+    SampleRate = 0
+    Data = []
+    Multiplier = 0
+
+    totalSize = 0
+    chunkSize = 0
+    Format = 0
+    isASFF = False
+    sampSize = 0
+    payloadSize = 0
+    actualDataSize = 0
