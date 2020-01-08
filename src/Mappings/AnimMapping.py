a79ad1b2d2101782b6366371be95d47791a03a53
Marek Grzyb <grzybdev@gmail.com>
2020-01-08 18:04:12 +0100

    Added standard methods for dictionaries

--- src/Mappings/AnimMapping.py
+++ src/Mappings/AnimMapping.py
@@ -6,10 +6,32 @@ class AnimSetMapping(object):
         self.map = []
 
     def Get(self, set, anim):
+        pass
+
+    def __Add(self, category, name, palette="Std.Palette", skipNormalMap=False, addBorder=0, allowRealtimePalette=False):
+        entry = Entry()
+
+        entry.AddBorder = addBorder
+        entry.AllowRealtimePalette = allowRealtimePalette
+        entry.Category = category
+        entry.Name = name
+        entry.Palette = palette
+        entry.SkipNormalMap = skipNormalMap
+
+        self.tempSet.append(entry)
+
+    def __NextSet(self):
+        self.map.append(self.tempSet)
+        self.tempSet = []
+
+    def Get(self, setID, animID):
         try:
-            return self.map[set][anim]
+            return self.map[setID][animID]
         except IndexError:
-            return Entry()
+            entry = Entry()
+            entry.Category = "Unidentified"
+            entry.Name = str(setID) + "_" + str(animID)
+            return entry
 
 
 animMapping = AnimSetMapping("anim")
