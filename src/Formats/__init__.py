973559c597db6d2d8dff72cc96f72edf9463141b
Marek Grzyb <grzybdev@gmail.com>
2020-01-14 13:54:41 +0100

    Imported MusicConverter in __init__

--- src/Formats/__init__.py
+++ src/Formats/__init__.py
@@ -2,3 +2,4 @@ from src.Formats.Language import LanguageConverter
 from src.Formats.Data import DataConverter
 from src.Formats.Anims import AnimsConverter
 from src.Formats.Episode import EpisodeConverter
+from src.Formats.Music import MusicConverter
