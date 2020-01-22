b908e29b155e65f691e91f8cfc667374740cb0f1
Marek Grzyb <grzybdev@gmail.com>
2020-01-22 10:36:46 +0100

    Added LayerSection level DataClass

---
+++ src/DataClasses/Level/LayerSection.py
@@ -0,0 +1,27 @@
+class LayerSection:
+    Flags = 0
+    Type = 0
+    Used = False
+
+    Width = 0
+    InternalWidth = 0
+    Height = 0
+    Depth = 0
+
+    DetailLevel = 0
+
+    WaveX = 0
+    WaveY = 0
+
+    SpeedX = 0
+    SpeedY = 0
+
+    AutoSpeedX = 0
+    AutoSpeedY = 0
+
+    TexturedBackgroundType = 0
+    TexturedParams1 = 0
+    TexturedParams2 = 0
+    TexturedParams3 = 0
+
+    Tiles = []
