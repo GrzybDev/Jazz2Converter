678cd3aa0d9d88ae921514b6c1545e81496eebb5
Marek Grzyb <grzybdev@gmail.com>
2020-01-26 17:09:59 +0100

    Added WeaponType mapping

---
+++ src/Mappings/WeaponType.py
@@ -0,0 +1,14 @@
+from enum import IntEnum
+
+
+class WeaponType(IntEnum):
+    Blaster = 0
+    Bouncer = 1
+    Freezer = 2
+    Seeker = 3
+    RF = 4
+    Toaster = 5
+    TNT = 6
+    Pepper = 7
+    Electro = 8
+    Thunderbolt = 9
