7e6dfb971f63a8ecbb812b43aa1362c54b3f1374
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:40:39 +0100

    Optimized and reformatted DataConverter

--- src/Compatibility/Data.py
+++ src/Compatibility/Data.py
@@ -1,59 +1,70 @@
-import logging
 import zlib
 
-from src.DataClasses.Data import *
-from src.Helpers.logger import *
-from src.Mappings import fileTypes
-from src.Utilities import FileConverter
+from src.Compatibility.Assets.Palette import PaletteDataFile
+from src.Compatibility.Assets.Picture import PictureDataFile
+from src.Compatibility.Assets.SoundFXList import SoundFXList
+from src.Compatibility.Assets.Texture import TextureDataFile
+from src.DataClasses.Data.File import DataFile
+from src.Logger import error, warning, verbose, info
+from src.Utilities.DataBlock import DataBlock
+from src.Utilities.FileConverter import FileConverter
+
+knownFileTypes = {
+    0x23: PictureDataFile,
+    0x18: PictureDataFile,
+    0x1D: PictureDataFile,
+    0x17: PictureDataFile,
+    0x16: PictureDataFile,
+    0x19: PictureDataFile,
+    0x1A: PictureDataFile,
+    0x15: PictureDataFile,
+    0x52: PictureDataFile,
+    0x9: PaletteDataFile,
+    0xFFFFFFFF: PaletteDataFile,
+    0x11C9A10: SoundFXList,
+    0x11C8330: SoundFXList,
+    0x11C88A0: SoundFXList,
+    0x11C8750: SoundFXList,
+    0x11C8320: SoundFXList,
+    0x11C8AB0: TextureDataFile,
+}
 
 
 class DataConverter(FileConverter):
-
     def __init__(self, path):
         super().__init__(path)
 
-        self.magic = 0x0  # Should be: 0x42494C50 (PLIB)
-        self.signature = 0x0  # Should be 0xBEBAADDE
+        self.magic: int = 0x0  # Should be: 0x42494C50 (PLIB)
+        self.signature: int = 0x0  # Should be 0xBEBAADDE
 
-        self.version = 0
+        self.version: int = 0
 
-        self.recordedSize = 0
-        self.recordedCRC = 0
+        self.recordedSize: int = 0
+        self.recordedCRC: int = 0
 
-        self.headerBlockPackedSize = 0
-        self.headerBlockUnpackedSize = 0
-        self.headerBlock = ""
+        self.headerBlock: str = ""
 
         self.archiveFiles = []
-        self.headerEndOffset = 0
+        self.headerEndOffset: int = 0
 
-    def convert(self):
-        super().convert()
+    def _FileConverter__convert(self):
+        self.__checkFile()
+        self.__loadData()
 
-        try:
-            self.__checkFile()
-            self.__loadData()
-
-            self.__loadHeaderBlock()
-        except Exception as e:
-            logging.error(error("Unexpected error happened while converting file: " + self.path + "! (" + str(e) + ")"))
+        self.__loadHeaderBlock()
 
     def __checkFile(self):
         self.magic = self.file.ReadUInt()
 
         if self.magic != 0x42494C50:
-            logging.warning(warning("File " + self.path + " is not correct Jazz Jackrabbit 2 Data File!"
-                                                          " (Incorrect magic number) "
-                                                          "Skipping that file..."))
-            self.finish()
+            error("File " + self.path + " is not correct Jazz Jackrabbit 2 Data File! (Incorrect magic number)")
+            raise ValueError("Invalid magic number")
 
         self.signature = self.file.ReadUInt()
 
         if self.signature != 0xBEBAADDE:
-            logging.warning(warning("File " + self.path + " is not correct Jazz Jackrabbit 2 Data File!"
-                                                          " (Incorrect signature) "
-                                                          "Skipping that file..."))
-            self.finish()
+            error("File " + self.path + " is not correct Jazz Jackrabbit 2 Data File! (Incorrect signature)")
+            raise ValueError("Invalid signature")
 
     def __loadData(self):
         self.version = self.file.ReadUInt()
@@ -62,25 +73,17 @@ class DataConverter(FileConverter):
         self.recordedCRC = self.file.ReadUInt()
 
     def __loadHeaderBlock(self):
-        self.headerBlockPackedSize = self.file.ReadUInt()
-        self.headerBlockUnpackedSize = self.file.ReadUInt()
-
-        self.headerBlock = zlib.decompress(self.file.ReadBytes(self.headerBlockPackedSize))
-
-        if len(self.headerBlock) != self.headerBlockUnpackedSize:
-            logging.warning(warning("File " + self.path + " is not correct Jazz Jackrabbit 2 Data File!"
-                                                          " (Incorrect unpacked size) "
-                                                          "Skipping that file..."))
-            self.finish()
-        else:
-            self.headerBlock = DataBlock(self.headerBlock)
+        headerBlockPackedSize = self.file.ReadUInt()
+        headerBlockUnpackedSize = self.file.ReadUInt()
+        self.headerBlock = DataBlock(self.file.ReadBytes(headerBlockPackedSize),
+                                     headerBlockPackedSize, headerBlockUnpackedSize)
 
         while True:
             file = DataFile()
 
             file.name = self.headerBlock.ReadString(32, True)
 
-            if file.name == '':  # Most probably end of header block
+            if file.name == "":  # Most probably end of header block
                 break
 
             file.type = self.headerBlock.ReadUInt()
@@ -89,18 +92,16 @@ class DataConverter(FileConverter):
             file.filePackedSize = self.headerBlock.ReadUInt()
             file.fileUnpackedSize = self.headerBlock.ReadUInt()
 
-            logging.debug(verbose("Found file: " + file.name + " (type: " + str(hex(file.type)) + ") at offset " +
-                                  str(file.offset) + " (size: " + str(file.filePackedSize) + " bytes)"))
+            verbose("Found file: " + file.name + " (type: " + str(hex(file.type)) + ") at offset " + str(file.offset) +
+                    " (size: " + str(file.filePackedSize) + " bytes)")
 
             self.archiveFiles.append(file)
 
         self.headerEndOffset = self.file.context.tell()
 
-    def save(self, outputPath):
-        super().save(outputPath)
-
+    def _FileConverter__save(self, outputPath):
         for file in self.archiveFiles:
-            logging.debug(verbose("Decompressing " + file.name + "..."))
+            verbose("Decompressing " + file.name + "...")
             finalFilePath = outputPath + file.name
 
             self.file.context.seek(self.headerEndOffset + file.offset)
@@ -109,18 +110,18 @@ class DataConverter(FileConverter):
             decompressedFile = zlib.decompress(rawFile)
 
             if len(decompressedFile) != file.fileUnpackedSize:
-                logging.warning(warning("Failed to save file to " + finalFilePath + "! "
-                                        "That file won't be converted/saved..."))
+                warning("Failed to save file to " + finalFilePath + "! " +
+                        "That file won't be converted/saved...")
                 continue
 
-            fileConverter = fileTypes.get(file.type)
+            fileConverter = knownFileTypes.get(file.type)
 
             if fileConverter is None:
-                logging.warning(warning("Unknown file type: " + str(file.type) + "! "
-                                        "Will save raw file to " + finalFilePath))
+                warning("Unknown file type: " + str(file.type) + "! " +
+                        "Will save raw file to " + finalFilePath)
 
             with open(finalFilePath, "wb") as finalFile:
-                logging.info(info("Now saving file " + finalFilePath + "..."))
+                info("Now saving file " + finalFilePath + "...")
                 finalFile.write(decompressedFile)
                 finalFile.close()
 
