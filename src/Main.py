7459c0e421e10379042982eb134ba09fb943f850
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:43:26 +0100

    Reformatted Main executable file

--- src/Main.py
+++ src/Main.py
@@ -1,33 +1,34 @@
-import logging
 import getopt
-import shutil
+import logging
 import os.path
+import shutil
 
-from src.Helpers.errors import *
-from src.Helpers.logger import *
+from colorama import init
+
 from src.Converter import Converter
-
-logging.basicConfig(format="[%(module)s] %(message)s")
+from src.ErrorCodes import SUCCESS_OK, ERROR_INVALID_ARGUMENTS, ERROR_UNKNOWN_ARGUMENT, ERROR_NOT_VALID_GAME_FOLDER, \
+    ERROR_NO_GAME_FOLDER_SPECIFIED, ERROR_LAUNCHED_INCORRECTLY
+from src.Logger import info, warning, verbose, error
 
 
 def showHelp():
-    print(info("Usage: run.py [-h|--help] -i|--input \"GAME FOLDER\" (arguments)"))
-    print("\n")
-    print(info("-h | --help\t|| Shows this message"))
-    print(info("-v | --verbose\t|| Shows additional debug information"))
-    print(info("-i | --input\t|| Sets Jazz Jackrabbit 2 game folder"))
-    print(info("-o | --output\t|| Sets converted file path (Converter output folder)"))
-    print(info("-c | --clear\t|| Clears output folder before conversion (to prevent folder is not empty errors)"))
-    print("\n")
-    print(info("Converter options:"))
-    print(info("--skip-languages\t|| Skips language files (*.j2s)"))
-    print(info("--skip-data\t\t|| Skip data files (*.j2d)"))
-    print(info("--skip-animations\t|| Skip animation files (*.j2a)"))
-    print(info("--skip-episodes\t|| Skip episode files (*.j2e)"))
-    print(info("--skip-music\t|| Skip music files (*.j2b, *.mod, *.it, *.s3m)"))
-    print(info("--skip-videos\t|| Skip video files (*.j2v)"))
-    print(info("--skip-tilesets\t|| Skip tileset files (*.j2t)"))
-    print(info("--skip-levels\t|| Skip level files (*.j2l)"))
+    info('Usage: run.py [-h|--help] -i|--input "GAME FOLDER" (arguments)')
+    info("\n")
+    info("-h | --help\t|| Shows this message")
+    info("-v | --verbose\t|| Shows additional debug information")
+    info("-i | --input\t|| Sets Jazz Jackrabbit 2 game folder")
+    info("-o | --output\t|| Sets converted file path (Converter output folder)")
+    info("-c | --clear\t|| Clears output folder before conversion (to prevent \"folder is not empty\" errors)")
+    info("\n")
+    info("Converter options:")
+    info("--skip-languages\t|| Skips language files (*.j2s)")
+    info("--skip-data\t\t|| Skip data files (*.j2d)")
+    info("--skip-animations\t|| Skip animation files (*.j2a)")
+    info("--skip-episodes\t|| Skip episode files (*.j2e)")
+    info("--skip-music\t|| Skip music files (*.j2b, *.mod, *.it, *.s3m)")
+    info("--skip-videos\t|| Skip video files (*.j2v)")
+    info("--skip-tilesets\t|| Skip tileset files (*.j2t)")
+    info("--skip-levels\t|| Skip level files (*.j2l)")
 
     return SUCCESS_OK
 
@@ -42,24 +43,27 @@ def getBooleanFromUser(question):
             else:
                 return False
         else:
-            print(warning("Invalid reply!"))
+            warning("Invalid reply!")
+
 
 def clearFolder(output):
-    logging.info(info("Clearing output folder..."))
+    info("Clearing output folder...")
 
     try:
         shutil.rmtree(output)
-        logging.debug(verbose("Folder cleared successfully!"))
+        verbose("Folder cleared successfully!")
     except Exception as e:
-        logging.debug(verbose("There was an error during clearing folder! (" + str(e) + ")"))
+        verbose("There was an error during clearing folder! (" + str(e) + ")")
 
 
 def run(arguments):
+    init()
     logging.getLogger().setLevel(logging.INFO)
-    print(info("Jazz Jackrabbit 2 Converter v1.0"))
-    print(info("By Marek Grzyb (@GrzybDev) (https://github.com/GrzybDev)"))
-    print(info("Designed for Jazz Jackrabbit 2 v1.24"))
-    print(info("--------------------------------------------------------"))
+
+    info("Jazz Jackrabbit 2 Converter v1.0")
+    info("By Marek Grzyb (@GrzybDev) (https://github.com/GrzybDev)")
+    info("Designed for Jazz Jackrabbit 2 v1.24")
+    info("--------------------------------------------------------")
 
     converterArgs = {}
     gameFolder = None
@@ -75,53 +79,73 @@ def run(arguments):
             "--skip-music": lambda: converterArgs.update(skipMusic=True),
             "--skip-videos": lambda: converterArgs.update(skipVideos=True),
             "--skip-tilesets": lambda: converterArgs.update(skipTilesets=True),
-            "--skip-levels": lambda: converterArgs.update(skipLevels=True)
+            "--skip-levels": lambda: converterArgs.update(skipLevels=True),
         }
 
         try:
-            opts, args = getopt.getopt(arguments[1:], "hvi:o:c", ["help", "verbose", "input=", "output=", "clear",
-                                                                 "skip-languages", "skip-data", "skip-animations",
-                                                                 "skip-episodes", "skip-music", "skip-videos", 
-                                                                 "skip-tilesets", "skip-levels"])
+            opts, args = getopt.getopt(
+                arguments[1:],
+                "hvi:o:c",
+                [
+                    "help",
+                    "verbose",
+                    "input=",
+                    "output=",
+                    "clear",
+                    "skip-languages",
+                    "skip-data",
+                    "skip-animations",
+                    "skip-episodes",
+                    "skip-music",
+                    "skip-videos",
+                    "skip-tilesets",
+                    "skip-levels",
+                ],
+            )
         except getopt.GetoptError:
-            print(error("Invalid arguments provided!\n"
-                        "Run program without parameters to enter interactive mode or check --help for usage"))
+            error("Invalid arguments provided!\n"
+                  "Run program without parameters to enter interactive mode or check --help for usage")
             return ERROR_INVALID_ARGUMENTS
 
         for opt, arg in opts:
-            if opt in ('-h', "--help"):
+            if opt in ("-h", "--help"):
                 return showHelp()
-            elif opt in ('-v', "--verbose"):
+            elif opt in ("-v", "--verbose"):
                 logging.getLogger().setLevel(logging.DEBUG)
-            elif opt in ('-i', "--input"):
+            elif opt in ("-i", "--input"):
                 gameFolder = arg
-            elif opt in ('-o', "--output"):
+            elif opt in ("-o", "--output"):
                 outputFolder = arg
-            elif opt in ('-c', "--clear"):
+            elif opt in ("-c", "--clear"):
                 clearOutputFolder = True
-            elif opt in ("--skip-languages",
-                         "--skip-data",
-                         "--skip-animations",
-                         "--skip-episodes",
-                         "--skip-music",
-                         "--skip-videos",
-                         "--skip-tilesets",
-                         "--skip-levels"):
+            elif opt in (
+                    "--skip-languages",
+                    "--skip-data",
+                    "--skip-animations",
+                    "--skip-episodes",
+                    "--skip-music",
+                    "--skip-videos",
+                    "--skip-tilesets",
+                    "--skip-levels",
+            ):
                 knownArgs.get(opt)()
             else:
-                print(error("Got unknown argument: " + opt + "\n"
-                            "Run program without parameters to enter interactive mode or check --help for usage"))
+                error("Got unknown argument: " + opt +
+                      "\nRun program without parameters to enter interactive mode or check --help for usage")
                 return ERROR_UNKNOWN_ARGUMENT
 
         if gameFolder is not None or outputFolder is not None:
-            if os.path.exists(gameFolder) and (os.path.exists(gameFolder + "/Jazz2.exe")
-                                               and os.path.isfile(gameFolder + "/Jazz2.exe")):
+            if os.path.exists(gameFolder) and (
+                    os.path.exists(gameFolder + "/Jazz2.exe")
+                    and os.path.isfile(gameFolder + "/Jazz2.exe")
+            ):
                 pass
             else:
-                print(error("Folder you specified: " + gameFolder + " is not valid Jazz Jackrabbit 2 Game Folder!"))
+                error("Folder you specified: " + gameFolder +
+                      " is not valid Jazz Jackrabbit 2 Game Folder!")
                 return ERROR_NOT_VALID_GAME_FOLDER
         else:
-            print(error("You didn't specified game or output folder!"))
+            error("You didn't specified game or output folder!")
             return ERROR_NO_GAME_FOLDER_SPECIFIED
     else:
         validGameFolder = False
@@ -129,16 +153,20 @@ def run(arguments):
         while not validGameFolder:
             gameFolder = input("Please enter Jazz Jackrabbit 2 game directory: ")
 
-            if os.path.exists(gameFolder) and (os.path.exists(gameFolder + "/Jazz2.exe")
-                                               and os.path.isfile(gameFolder + "/Jazz2.exe")):
+            if os.path.exists(gameFolder) and (
+                    os.path.exists(gameFolder + "/Jazz2.exe")
+                    and os.path.isfile(gameFolder + "/Jazz2.exe")
+            ):
                 validGameFolder = True
             else:
-                print(warning("Folder you specified: " + gameFolder + " is not valid Jazz Jackrabbit 2 Game Folder!"))
+                warning("Folder you specified: " + gameFolder +
+                        " is not valid Jazz Jackrabbit 2 Game Folder!")
 
         while outputFolder is None:
             outputFolder = input("Please enter path where to put converted files: ")
 
         clearOutputFolder = getBooleanFromUser("Clear output folder?")
+
         converterArgs.update(skipLangs=not getBooleanFromUser("Convert language files (*.j2s)?"))
         converterArgs.update(skipData=not getBooleanFromUser("Convert data files (*.j2d)?"))
         converterArgs.update(skipAnims=not getBooleanFromUser("Convert animation files (*.j2a)?"))
@@ -156,4 +184,5 @@ def run(arguments):
 
 
 if __name__ == "__main__":
-    print(error("You have to run Jazz2Converter using run.py located in root directory!"))
+    error("You have to run Jazz2Converter using run.py located in root directory!")
+    exit(ERROR_LAUNCHED_INCORRECTLY)
