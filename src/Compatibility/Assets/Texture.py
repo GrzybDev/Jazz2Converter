2cc8aa9e301c58417dbd625ffeb5a5076c47b133
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:40:28 +0100

    Optimized and reformatted TextureDataFile

--- src/Compatibility/Assets/Texture.py
+++ src/Compatibility/Assets/Texture.py
@@ -1,14 +1,11 @@
-import logging
 import os
 
 from PIL import Image
 
-from src.Helpers.logger import *
-from src.Utilities import FileConverter
+from src.Utilities.FileConverter import FileConverter
 
 
 class TextureDataFile(FileConverter):
-
     def __init__(self, path):
         super().__init__(path)
 
@@ -17,22 +14,15 @@ class TextureDataFile(FileConverter):
         self.image = Image.new("RGBA", [int(self.width), int(self.height)], 255)
         self.imageData = self.image.load()
 
-    def convert(self):
-        super().convert()
+    def _FileConverter__convert(self):
+        for x in range(self.image.size[1]):
+            for y in range(self.image.size[0]):
+                imageByte = self.file.ReadByte()
 
-        try:
-            for x in range(self.image.size[1]):
-                for y in range(self.image.size[0]):
-                    imageByte = self.file.ReadByte()
-
-                    self.imageData[y, x] = (imageByte, imageByte, imageByte, imageByte)
-        except Exception as e:
-            logging.error(error("Unexpected error happened while converting file: " + self.path + "! (" + str(e) + ")"))
+                self.imageData[y, x] = (imageByte, imageByte, imageByte, imageByte)
 
         self.finish()
 
-    def save(self, outputPath):
-        super().save(outputPath)
-
+    def _FileConverter__save(self, outputPath):
         self.image.save(outputPath + ".png")
         os.remove(outputPath)
