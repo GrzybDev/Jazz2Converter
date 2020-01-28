975e79d375fd78151d56348b22de66154c97a903
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:41:20 +0100

    Optimized and reformatted TilesetConverter

--- src/Compatibility/Tileset.py
+++ src/Compatibility/Tileset.py
@@ -1,147 +1,97 @@
-import logging
 import os
-import zlib
 
 from PIL import Image
 
-from src.Helpers.logger import *
-from src.Utilities import FileConverter
-from src.DataClasses.Data import *
-from src.DataClasses.Tileset import *
+from src.DataClasses.Color import Color
+from src.DataClasses.Tileset.TileSection import TileSection
+from src.Logger import error
+from src.Utilities.DataBlock import DataBlock
+from src.Utilities.FileConverter import FileConverter
 
 
 class TilesetConverter(FileConverter):
-
     def __init__(self, path):
         super().__init__(path)
 
-        self.name = ""
-        self.version = 0
+        self.name: str = ""
         self.palette = []
         self.tiles = []
-        self.tileCount = 0
-        self.maxTilesCount = 0
+        self.tileCount: int = 0
+        self.maxTilesCount: int = 0
 
-        self.recordedSize = 0
-        self.recordedCRC = 0
-
-        self.infoBlockPackedSize = 0
-        self.infoBlockUnpackedSize = 0
         self.infoBlock = None
-        
-        self.imageBlockPackedSize = 0
-        self.imageBlockUnpackedSize = 0
         self.imageBlock = None
-
-        self.alphaBlockPackedSize = 0
-        self.alphaBlockUnpackedSize = 0
         self.alphaBlock = None
-
-        self.maskBlockPackedSize = 0
-        self.maskBlockUnpackedSize = 0
         self.maskBlock = None
 
-    def convert(self):
-        super().convert()
+    def _FileConverter__convert(self):
+        self.file.ReadBytes(180)  # Skip copyright notice
 
-        try:
-            self.file.ReadBytes(180)  # Skip copyright notice
+        self.__readHeader()
 
-            self.__readHeader()
-
-            self.__LoadMetadata()
-            self.__LoadImageData()
-            self.__LoadMaskData()
-        except Exception as e:
-            logging.error(error("Unexpected error happened during conversion! (" + str(e) + ")"))
-            self.finish()
+        self.__LoadMetadata()
+        self.__LoadImageData()
+        self.__LoadMaskData()
 
     def __readHeader(self):
-        headerBlock = DataBlock(self.file.ReadBytes(82))
+        headerBlock = DataBlock(self.file.ReadBytes(82), 82)
 
         magicValue = headerBlock.ReadUInt()
         signature = headerBlock.ReadUInt()
 
-        if magicValue == 0x454C4954 and signature == 0xAFBEADDE:
-            self.name = headerBlock.ReadString(32, True)
-            self.version = headerBlock.ReadUShort()
-            self.maxTilesCount = 1024 if self.version <= 512 else 4096
-            
-            self.recordedSize = headerBlock.ReadUInt()
-            self.recordedCRC = headerBlock.ReadUInt()
+        if magicValue == 0x454C4954:
+            error("Invalid magic value (Expected " + str(0x454C4954) + ", but got " + str(magicValue) + ")")
+            raise ValueError("Invalid magic value")
 
-            self.infoBlockPackedSize = headerBlock.ReadUInt()
-            self.infoBlockUnpackedSize = headerBlock.ReadUInt()
-            self.imageBlockPackedSize = headerBlock.ReadUInt()
-            self.imageBlockUnpackedSize = headerBlock.ReadUInt()
-            self.alphaBlockPackedSize = headerBlock.ReadUInt()
-            self.alphaBlockUnpackedSize = headerBlock.ReadUInt()
-            self.maskBlockPackedSize = headerBlock.ReadUInt()
-            self.maskBlockUnpackedSize = headerBlock.ReadUInt()
+        if signature == 0xAFBEADDE:
+            error("Invalid signature (Expected " + str(0xAFBEADDE) + ", but got " + str(signature) + ")")
+            raise ValueError("Invalid signature")
 
-            self.infoBlock = self.file.ReadString(self.infoBlockPackedSize)
-            self.imageBlock = self.file.ReadString(self.imageBlockPackedSize)
-            self.alphaBlock = self.file.ReadString(self.alphaBlockPackedSize)
-            self.maskBlock = self.file.ReadString(self.maskBlockPackedSize)
+        self.name = headerBlock.ReadString(32, True)
+        version = headerBlock.ReadUShort()
+        self.maxTilesCount = 1024 if version <= 512 else 4096
 
-            if len(self.infoBlock) != self.infoBlockPackedSize \
-                    or len(self.imageBlock) != self.imageBlockPackedSize \
-                    or len(self.alphaBlock) != self.alphaBlockPackedSize \
-                    or len(self.maskBlock) != self.maskBlockPackedSize:
-                logging.error(error("File is incomplete or corrupted!"))
-                self.finish()
-                return
+        recordedSize = headerBlock.ReadUInt()
+        recordedCRC = headerBlock.ReadUInt()
 
-            self.infoBlock = zlib.decompress(self.infoBlock)
-            self.imageBlock = zlib.decompress(self.imageBlock)
-            self.alphaBlock = zlib.decompress(self.alphaBlock)
-            self.maskBlock = zlib.decompress(self.maskBlock)
+        infoBlockPackedSize = headerBlock.ReadUInt()
+        infoBlockUnpackedSize = headerBlock.ReadUInt()
+        imageBlockPackedSize = headerBlock.ReadUInt()
+        imageBlockUnpackedSize = headerBlock.ReadUInt()
+        alphaBlockPackedSize = headerBlock.ReadUInt()
+        alphaBlockUnpackedSize = headerBlock.ReadUInt()
+        maskBlockPackedSize = headerBlock.ReadUInt()
+        maskBlockUnpackedSize = headerBlock.ReadUInt()
 
-            if len(self.infoBlock) != self.infoBlockUnpackedSize \
-                    or len(self.imageBlock) != self.imageBlockUnpackedSize \
-                    or len(self.alphaBlock) != self.alphaBlockUnpackedSize \
-                    or len(self.maskBlock) != self.maskBlockUnpackedSize:
-                logging.error(error("Incorrect block sizes after decompression!"))
-                self.finish()
-                return
-
-            self.infoBlock = DataBlock(self.infoBlock)
-            self.imageBlock = DataBlock(self.imageBlock)
-            self.alphaBlock = DataBlock(self.alphaBlock)
-            self.maskBlock = DataBlock(self.maskBlock)
-        else:
-            logging.error(error("Tileset header have invalid magic number or signature!"))
-            self.finish()
+        self.infoBlock = DataBlock(self.file.ReadBytes(infoBlockPackedSize),
+                                   infoBlockPackedSize, infoBlockUnpackedSize)
+        self.imageBlock = DataBlock(self.file.ReadBytes(imageBlockPackedSize),
+                                    imageBlockPackedSize, imageBlockUnpackedSize)
+        self.alphaBlock = DataBlock(self.file.ReadBytes(alphaBlockPackedSize),
+                                    alphaBlockPackedSize, alphaBlockUnpackedSize)
+        self.maskBlock = DataBlock(self.file.ReadBytes(maskBlockPackedSize),
+                                   maskBlockPackedSize, maskBlockUnpackedSize)
 
     def __LoadMetadata(self):
-        for i in range(256):
-            self.palette.append({
-                "R": self.infoBlock.ReadByte(),
-                "G": self.infoBlock.ReadByte(),
-                "B": self.infoBlock.ReadByte(),
-                "A": 255 - self.infoBlock.ReadByte()
-            })
+        self.palette = [Color(self.infoBlock.ReadByte(), self.infoBlock.ReadByte(), self.infoBlock.ReadByte(),
+                              255 - self.infoBlock.ReadByte()) for each in range(256)]
 
         self.tileCount = self.infoBlock.ReadUInt()
+        self.tiles = [TileSection() for each in range(self.maxTilesCount)]
 
-        for i in range(self.maxTilesCount):
-            self.tiles.append(TileSection())
-            self.tiles[i].Opaque = self.infoBlock.ReadBool()
+        for tile in self.tiles: tile.Opaque = self.infoBlock.ReadBool()
 
         self.infoBlock.DiscardBytes(self.maxTilesCount)  # Block of unknown bytes, skip
 
-        for i in range(self.maxTilesCount):
-            self.tiles[i].ImageDataOffset = self.infoBlock.ReadUInt()
+        for tile in self.tiles: tile.ImageDataOffset = self.infoBlock.ReadUInt()
 
         self.infoBlock.DiscardBytes(4 * self.maxTilesCount)  # Block of unknown bytes, skip
 
-        for i in range(self.maxTilesCount):
-            self.tiles[i].AlphaDataOffset = self.infoBlock.ReadUInt()
+        for tile in self.tiles: tile.AlphaDataOffset = self.infoBlock.ReadUInt()
 
         self.infoBlock.DiscardBytes(4 * self.maxTilesCount)  # Block of unknown bytes, skip
 
-        for i in range(self.maxTilesCount):
-            self.tiles[i].MaskDataOffset = self.infoBlock.ReadUInt()
+        for tile in self.tiles: tile.MaskDataOffset = self.infoBlock.ReadUInt()
 
         self.infoBlock.DiscardBytes(4 * self.maxTilesCount)
 
@@ -158,12 +108,20 @@ class TilesetConverter(FileConverter):
             for i in range(blockSize * blockSize):
                 idx = imageData[i]
 
-                if len(alphaMaskData) > 0 and ((alphaMaskData[int(i / 8)] >> (i % 8)) & 0x01) == 0x00:
+                if (
+                        len(alphaMaskData) > 0
+                        and ((alphaMaskData[int(i / 8)] >> (i % 8)) & 0x01) == 0x00
+                ):
                     color = self.palette[0]
                 else:
                     color = self.palette[idx]
 
-                image[i % blockSize, i / blockSize] = (color["R"], color["G"], color["B"], color["A"])
+                image[i % blockSize, i / blockSize] = (
+                    color.r,
+                    color.g,
+                    color.b,
+                    color.a,
+                )
 
     def __LoadMaskData(self):
         blockSize = 32
@@ -181,24 +139,42 @@ class TilesetConverter(FileConverter):
                     pixelIdx = 8 * i + j
 
                     if ((idx >> j) & 0x01) == 0:
-                        mask[pixelIdx % blockSize, pixelIdx / blockSize] = (0, 0, 0, 0)  # Transparent
+                        mask[pixelIdx % blockSize, pixelIdx / blockSize] = (
+                            0, 0, 0, 0)  # Transparent
                     else:
-                        mask[pixelIdx % blockSize, pixelIdx / blockSize] = (0, 0, 0, 255)  # Black
+                        mask[pixelIdx % blockSize, pixelIdx / blockSize] = (
+                            0, 0, 0, 255)  # Black
 
-    def save(self, outputPath):
-        super().save(outputPath)
-
+    def _FileConverter__save(self, outputPath):
         tileSize = 32
         tilesPerRow = 30
 
-        tilesTexture = Image.new("RGBA", [tileSize * tilesPerRow, int(((self.tileCount - 1) / tilesPerRow + 1)) * tileSize])
-        masksTexture = Image.new("RGBA", [tileSize * tilesPerRow, int(((self.tileCount - 1) / tilesPerRow + 1)) * tileSize])
+        tilesTexture = Image.new(
+            "RGBA",
+            [
+                tileSize * tilesPerRow,
+                int(((self.tileCount - 1) / tilesPerRow + 1)) * tileSize,
+            ],
+        )
+        masksTexture = Image.new(
+            "RGBA",
+            [
+                tileSize * tilesPerRow,
+                int(((self.tileCount - 1) / tilesPerRow + 1)) * tileSize,
+            ],
+        )
 
         for i in range(self.maxTilesCount):
             tile = self.tiles[i]
 
-            tilesTexture.paste(tile.Image, ((i % tilesPerRow) * tileSize, int(i / tilesPerRow) * tileSize))
-            masksTexture.paste(tile.Mask, ((i % tilesPerRow) * tileSize, int(i / tilesPerRow) * tileSize))
+            tilesTexture.paste(
+                tile.Image,
+                ((i % tilesPerRow) * tileSize, int(i / tilesPerRow) * tileSize),
+            )
+            masksTexture.paste(
+                tile.Mask,
+                ((i % tilesPerRow) * tileSize, int(i / tilesPerRow) * tileSize),
+            )
 
         outputPath = outputPath + os.path.splitext(os.path.basename(self.path))[0] + "/"
         os.mkdir(outputPath)
