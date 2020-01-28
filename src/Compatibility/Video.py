30201373cc4ac41ea3682136be131df99b729a1f
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:41:28 +0100

    Optimized and reformatted VideoConverter

--- src/Compatibility/Video.py
+++ src/Compatibility/Video.py
@@ -1,47 +1,40 @@
 import os
-import logging
-import tempfile
 import subprocess
+import tempfile
 import zlib
 
 from PIL import Image
 
-from src.Helpers.logger import *
-from src.Utilities import FileConverter
-from src.DataClasses.Video import *
-from src.DataClasses.Data import *
+from src.DataClasses.Video.Block import VideoBlock
+from src.Logger import verbose, error, info
+from src.Utilities.DataBlock import DataBlock
+from src.Utilities.FileConverter import FileConverter
+
 
 class VideoConverter(FileConverter):
-
     def __init__(self, path):
         super().__init__(path)
 
         self.FileSize = 0
-        self.CRC32 = 0 # Of the lowercase filename
+        self.CRC32 = 0  # Of the lowercase filename
 
         self.Width = 0
         self.Height = 0
 
         self.BitsPerPixel = 0
-        self.DelayBetweenFrames = 0 # In milliseconds
+        self.DelayBetweenFrames = 0  # In milliseconds
         self.TotalFrames = 0
 
         self.CompressedLength = 0
 
         self.BlocksLength = []
         self.Blocks = []
-    
-    def convert(self):
-        super().convert()
 
-        try:
-            self.__ReadHeader()
-            self.__ReadBlocks()
+    def _FileConverter__convert(self):
+        self.__ReadHeader()
+        self.__ReadBlocks()
 
-            self.__ExtractFrames()
-        except Exception as e:
-            logging.error(error("Unexpected error happened during conversion! (" + str(e) + ")"))
-            self.finish()
+        self.__ExtractFrames()
 
     def __ReadHeader(self):
         magic = self.file.ReadString(8).decode()
@@ -49,7 +42,7 @@ class VideoConverter(FileConverter):
         if magic == "CineFeed":
             self.FileSize = self.file.ReadUInt()
             self.CRC32 = self.file.ReadUInt()
-            self.Width = self.file.ReadUInt() 
+            self.Width = self.file.ReadUInt()
             self.Height = self.file.ReadUInt()
             self.BitsPerPixel = self.file.ReadUShort()
             self.DelayBetweenFrames = self.file.ReadUShort()
@@ -60,41 +53,40 @@ class VideoConverter(FileConverter):
 
             self.CompressedLength = self.file.ReadUInt()
 
-            logging.debug(verbose(str(self.Width) + "x" + str(self.Height) + " " + str(self.BitsPerPixel) + " bits per pixel. " +
-                          str(self.TotalFrames) + " frames. "
-                          "Delay between frames: " + str(self.DelayBetweenFrames)))
+            verbose(str(self.Width) + "x" + str(self.Height) + " " + str(self.BitsPerPixel) + " bits per pixel. " +
+                    str(self.TotalFrames) + " frames. Delay between frames: " + str(self.DelayBetweenFrames))
         else:
-            logging.error(error("File has invalid magic string! Expected CineFeed but got: " + magic))
-            self.finish()
-    
+            error("File has invalid magic string! Expected CineFeed but got: " + magic)
+            raise ValueError("Invalid magic number!")
+
     def __ReadBlocks(self):
         currentPosition = self.file.context.tell()
-        self.file.context.seek(0, 2) # Seek to end of the file
-        fileLength = self.file.context.tell() # Save file length
-        self.file.context.seek(currentPosition) # Revert position
+        self.file.context.seek(0, 2)  # Seek to end of the file
+        fileLength = self.file.context.tell()  # Save file length
+        self.file.context.seek(currentPosition)  # Revert position
 
         for i in range(4):
             self.Blocks.append(VideoBlock(i))
-        
+
+        rawData = ""
         while self.file.context.tell() != fileLength:
             for block in self.Blocks:
                 block.CompressedLength = self.file.ReadUInt()
-                block.Data += self.file.ReadBytes(block.CompressedLength)
+                rawData += self.file.ReadBytes(block.CompressedLength)
                 block.DataLength += block.CompressedLength
 
         for block in self.Blocks:
-            if len(block.Data) == block.DataLength:
+            if len(rawData) == block.DataLength:
                 decompressor = zlib.decompressobj()
-                data = decompressor.decompress(block.Data)
+                data = decompressor.decompress(rawData)
                 data += decompressor.flush()
-                block.Data = DataBlock(data)
+                block.Data = DataBlock(data, len(data))
             else:
-                logging.error(error("Read " + str(len(block.Data)) + " bytes, but expected " + str(block.CompressedLength)))
+                error("Read " + str(len(rawData)) + " bytes, but expected " + str(block.CompressedLength))
                 break
-    
+
     def __ExtractFrames(self):
         frame = Image.new("P", [self.Width, self.Height], 0)
-        frameID = 0
         palette = []
         self.tempFramesDir = tempfile.TemporaryDirectory()
 
@@ -109,10 +101,12 @@ class VideoConverter(FileConverter):
                 palette = []
 
                 for c in range(256):
-                    palette = palette + [self.Blocks[3].Data.ReadByte(),
-                                         self.Blocks[3].Data.ReadByte(),
-                                         self.Blocks[3].Data.ReadUShort()]
-            
+                    palette = palette + [
+                        self.Blocks[3].Data.ReadByte(),
+                        self.Blocks[3].Data.ReadByte(),
+                        self.Blocks[3].Data.ReadUShort(),
+                    ]
+
             for y in range(self.Height):
                 c = self.Blocks[0].Data.ReadByte()
                 x = 0
@@ -122,36 +116,45 @@ class VideoConverter(FileConverter):
                         u = self.Blocks[0].Data.ReadUShort() if c == 0 else c
 
                         for i in range(u):
-                            pixels[(y * self.Width) + x] = self.Blocks[3].Data.ReadByte()
+                            pixels[(y * self.Width) + x] = self.Blocks[
+                                3
+                            ].Data.ReadByte()
                             x += 1
                     else:
                         u = self.Blocks[0].Data.ReadUShort() if c == 0x81 else c - 106
-                        n = self.Blocks[1].Data.ReadUShort() + (self.Blocks[2].Data.ReadByte() + y - 127) * self.Width
+                        n = (
+                                self.Blocks[1].Data.ReadUShort()
+                                + (self.Blocks[2].Data.ReadByte() + y - 127) * self.Width
+                        )
 
                         for i in range(u):
                             pixels[(y * self.Width) + x] = copy[n]
                             x += 1
                             n += 1
 
-            frame = Image.frombytes(frame.mode, (frame.width, frame.height), bytes(pixels))
+            frame = Image.frombytes(
+                frame.mode, (frame.width, frame.height), bytes(pixels)
+            )
             frame.putpalette(palette)
             frame.save(self.tempFramesDir.name + "/" + str(frameID) + ".bmp")
-    
-    def save(self, outputPath):
-        super().save(outputPath)
 
-        try:
-            defaultDuration = self.TotalFrames / 25
-            correctDuration = self.DelayBetweenFrames * self.TotalFrames / 1000
-            correction = 1 + (correctDuration - defaultDuration) / defaultDuration
+    def _FileConverter__save(self, outputPath):
+        defaultDuration = self.TotalFrames / 25
+        correctDuration = self.DelayBetweenFrames * self.TotalFrames / 1000
+        correction = 1 + (correctDuration - defaultDuration) / defaultDuration
 
-            logging.info("Now optimizing video file using FFMpeg...")
-            subprocess.call(["ffmpeg",
-                             "-i", self.tempFramesDir.name + "/%d.bmp",
-                             "-pix_fmt", "yuv420p",
-                             "-filter:v", "setpts=" + str(correction) + "*PTS,fps=60",
-                             outputPath + os.path.splitext(os.path.basename(self.path))[0] + ".mp4"])
-        except FileNotFoundError:
-            logging.error(error("FFMpeg is not accessible, please install it system-wise or place it in current folder!"))
-        except Exception as e:
-            logging.error(error("Unexpected error happened during optimization! (" + str(e) + ")"))
+        info("Now optimizing video file using FFMpeg...")
+        subprocess.call(
+            [
+                "ffmpeg",
+                "-i",
+                self.tempFramesDir.name + "/%d.bmp",
+                "-pix_fmt",
+                "yuv420p",
+                "-filter:v",
+                "setpts=" + str(correction) + "*PTS,fps=60",
+                outputPath
+                + os.path.splitext(os.path.basename(self.path))[0]
+                + ".mp4",
+            ]
+        )
