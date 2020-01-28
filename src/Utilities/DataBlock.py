2a5a78dd98fdd6ce2adb1e0acb488a6af040bfda
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:38:20 +0100

    Re-formatted DataBlock utility

--- src/Utilities/DataBlock.py
+++ src/Utilities/DataBlock.py
@@ -1,32 +1,40 @@
+import zlib
 from io import BytesIO
-
 from struct import unpack
+
+from src.Logger import error
 
 
 class DataBlock:
+    def __init__(self, data, dataLength, uncompressedLength=0):
+        if uncompressedLength == 0:
+            self.context = BytesIO(data)
+        else:
+            if len(data) != dataLength:
+                error("Invalid DataBlock length! "
+                      "(Expected " + str(dataLength) + " bytes, but got: " + str(len(data)) + " bytes)!")
+                raise ValueError("Invalid DataBlock length!")
 
-    def __init__(self, data):
-        self.context = BytesIO(data)
+            dec = zlib.decompress(data)
 
-    def ReadString(self, length, trimToNull=False):
-        result = self.context.read(length)
+            if len(dec) != uncompressedLength:
+                error("Invalid decompressed datablock length! "
+                      "(Expected " + str(uncompressedLength) + " bytes, but got: " + str(len(dec)) + "bytes)!")
+                raise ValueError("Invalid decompressed DataBlock length!")
 
-        if trimToNull:
-            result = result.split(b"\x00")[0]
+            self.context = BytesIO(dec)
 
-        return result.decode()
+    def DiscardBytes(self, count):
+        self.context.read(count)
 
-    def ReadUInt(self):
-        return unpack("I", self.context.read(4))[0]
-
-    def ReadShort(self):
-        return unpack("h", self.context.read(2))[0]
-
-    def ReadUShort(self):
-        return unpack("H", self.context.read(2))[0]
+    def ReadBool(self):
+        return unpack("?", self.context.read(1))[0]
 
     def ReadByte(self):
         return unpack("B", self.context.read(1))[0]
+
+    def ReadEncodedFloat(self):
+        return unpack("i", self.context.read(4))[0] * 1.0 / 65536
 
     def ReadRawBytes(self, count, fromOffset=None):
         if fromOffset is not None:
@@ -39,11 +47,19 @@ class DataBlock:
 
         return bytesArray
 
-    def DiscardBytes(self, count):
-        self.context.read(count)
+    def ReadShort(self):
+        return unpack("h", self.context.read(2))[0]
 
-    def ReadBool(self):
-        return unpack("?", self.context.read(1))[0]
+    def ReadString(self, length, trimToNull=False):
+        result = self.context.read(length)
 
-    def ReadEncodedFloat(self):
-        return unpack("i", self.context.read(4))[0] * 1.0 / 65536
+        if trimToNull:
+            result = result.split(b"\x00")[0]
+
+        return result.decode()
+
+    def ReadUInt(self):
+        return unpack("I", self.context.read(4))[0]
+
+    def ReadUShort(self):
+        return unpack("H", self.context.read(2))[0]
