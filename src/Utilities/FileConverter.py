b1136e4116d4a241080523aa13688518e96fb708
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:37:20 +0100

    Re-formatted FileConverter

--- src/Utilities/FileConverter.py
+++ src/Utilities/FileConverter.py
@@ -1,25 +1,41 @@
-import logging
 from abc import ABC, abstractmethod
 
-from src.Helpers.logger import info
-from src.Utilities import File
+from src.Logger import info, error
+from src.Utilities.File import File
 
 
 class FileConverter(ABC):
-
     def __init__(self, path):
         self.file = File(open(path, "rb"))
         self.path = path
 
         super().__init__()
 
-    @abstractmethod
     def convert(self):
-        logging.info(info("Now converting " + self.path + "..."))
+        try:
+            info("Now converting " + self.path + "...")
+            self.__convert()
+        except Exception as e:
+            error("Unexpected error happened while converting file: " + self.path + "! " +
+                  "(" + str(e) + ")")
+            self.finish()
+
+    @abstractmethod
+    def __convert(self):
+        pass
 
     def finish(self):
         self.file.context.close()
 
-    @abstractmethod
     def save(self, outputPath):
-        logging.info(info("Finished conversion. Now saving to " + outputPath + "..."))
+        try:
+            info("Finished conversion. Now saving to " + outputPath + "...")
+            self.__save(outputPath)
+        except Exception as e:
+            error("Unexpected error happened during extraction process of file: " + self.path + "!" +
+                  "(" + str(e) + ")")
+            self.finish()
+
+    @abstractmethod
+    def __save(self, outputPath):
+        pass
