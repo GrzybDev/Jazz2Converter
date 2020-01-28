bd6d245bec9d172cfca823ac4343f00dcd79d47e
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:35:24 +0100

    Decorated VideoBlock DataClass

--- src/DataClasses/Video/Block.py
+++ src/DataClasses/Video/Block.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class VideoBlock:
-    ID = 0
+    ID: int = 0
     DataLength = 0
     CompressedLength = 0
     Data = b""
-
-    def __init__(self, _id):
-        self.ID = _id
