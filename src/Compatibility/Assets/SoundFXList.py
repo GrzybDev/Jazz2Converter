3408ac09ba9aa58faf1502c4762ae12ce99de337
Marek Grzyb <grzybdev@gmail.com>
2020-01-28 19:40:20 +0100

    Optimized and reformatted SoundFXList

--- src/Compatibility/Assets/SoundFXList.py
+++ src/Compatibility/Assets/SoundFXList.py
@@ -1,46 +1,34 @@
-import logging
 import json
 import os
 
-from src.Helpers.logger import *
-from src.Utilities import FileConverter
-from src.DataClasses.Data.Files import SoundFXListEntry
+from src.DataClasses.Data.Files.SoundFXListEntry import SoundFXListEntry
+from src.Utilities.FileConverter import FileConverter
 
 
 class SoundFXList(FileConverter):
-
     def __init__(self, path):
         super().__init__(path)
 
         self.soundFXList = []
 
-    def convert(self):
-        super().convert()
+    def _FileConverter__convert(self):
+        while True:
+            soundEvent = SoundFXListEntry(self.file.ReadUInt(), self.file.ReadUInt(),
+                                          self.file.ReadUInt(), self.file.ReadUInt())
 
-        try:
-            while True:
-                soundEvent = SoundFXListEntry()
+            if (
+                    soundEvent.Frame == 0xFFFFFFFF
+                    and soundEvent.Sample == 0x00000000
+                    and soundEvent.Volume == 0x00000000
+                    and soundEvent.Panning == 0x00000000
+            ):
+                break
 
-                soundEvent.frame = self.file.ReadUInt()
-                soundEvent.sample = self.file.ReadUInt()
-                soundEvent.volume = self.file.ReadUInt()
-                soundEvent.panning = self.file.ReadUInt()
-
-                if soundEvent.frame == 0xFFFFFFFF \
-                    and soundEvent.sample == 0x00000000 \
-                    and soundEvent.volume == 0x00000000 \
-                    and soundEvent.panning == 0x00000000:
-                    break
-
-                self.soundFXList.append(soundEvent.__dict__)
-        except Exception as e:
-            logging.error(error("Unexpected error happened while converting file: " + self.path + "! (" + str(e) + ")"))
+            self.soundFXList.append(soundEvent.__dict__)
 
         self.finish()
 
-    def save(self, outputPath):
-        super().save(outputPath)
-
+    def _FileConverter__save(self, outputPath):
         jsonDump = json.dumps(self.soundFXList)
 
         with open(outputPath + ".json", "w") as finalFile:
