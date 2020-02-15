631116821b8b3e55ae1ed2c5b8947791dade1e20
Marek Grzyb <grzybdev@gmail.com>
2020-02-15 20:04:19 +0100

    Forced disk sync after converting music and changed extension to .wav

--- src/Compatibility/Music.py
+++ src/Compatibility/Music.py
@@ -13,6 +13,11 @@ class MusicConverter(FileConverter):
     def _FileConverter__convert(self):
         info("Converting music file using openmpt123...")
         subprocess.call(["openmpt123", self.path, "--render"])
+        subprocess.call(["sync"])  # Warning: It might temporarily slow down computer
+        # I'm using forcing disk sync because FFMpeg can start reading files BEFORE it's being fully saved on disk.
+        # ...And that causes audio cut after some time
+
+        # On windows sync program are available here: https://docs.microsoft.com/sysinternals/downloads/sync
 
     def _FileConverter__save(self, outputPath):
         info("Now optimizing music file using FFMpeg...")
@@ -23,7 +28,7 @@ class MusicConverter(FileConverter):
                 self.path + ".wav",
                 outputPath
                 + os.path.splitext(os.path.basename(self.path))[0]
-                + ".ogg",
+                + ".wav",
             ]
         )
         os.remove(self.path + ".wav")
